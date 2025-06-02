6ac9eb7c09c44e3d1d4231bfc57d23776a52cf48
Krishna <Krishna@192.168.1.38>
2025-06-02 21:05:02 +0530

    Third commit

---
+++ samplemaven/src/main/java/encapsulation/EncapChild.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class EncapChild {
+
+	
+	
+	public static void main(String[] args) {
+		
+		EncapParent obj = new EncapParent();
+		obj.setter("Krishna", 30);
+		obj.getter();
+		
+	}
+	
+	
+}
